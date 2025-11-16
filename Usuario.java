83a1202d4e8e336f846cd0a938b5f974fcc2baa0
itzeelcontrerasss-png <itzeelcontrerasss@gmail.com>
2025-11-16 15:54:05 -0600

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,24 @@
+java
+
+public abstract class Usuario {
+  protected String id;
+  protected String nombre; 
+  protected String nickname;
+  protected String password; 
+
+public Usuario(String id, String nombre, String nickname, String correo, String password) {
+  this.id = id;
+  this.nombre = nombre;
+  this.nickname = nickname;
+  this.correo = correo; 
+  this.password = password; 
+}
+
+public String getId() { return id; }
+public String getNombre() { return nombre; }
+public String getNickname() { return nickname; }
+public String getCorreo() { return correo; }
+public String getPassword() { return password; }
+
+public abstract void mostrarMenu();
+}
