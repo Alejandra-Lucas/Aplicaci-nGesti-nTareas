8da412b297cae1e50a9f088289ce9a4950cd1b61
itzeelcontrerasss-png <itzeelcontrerasss@gmail.com>
2025-11-16 16:57:33 -0600

    Update Desarrollador.java

--- Desarrollador.java
+++ Desarrollador.java
@@ -1,1 +1,27 @@
+java
 
+  public class Desarrollador extends Usuario {
+
+    public Desarrollador(String id; String nombre; String nickname, String correo, String password)
+    {
+      super(id, nombre, nickname, correo, password);
+    }
+
+    @Override
+    public void mostrarMenu() {
+      System.out.println("===MENÚ DESARROLLADOR ===");
+      System.out.println("1. Crear tarea para mí");
+      System.out.println("2. Ver mis tareas");
+      System.out.println("3. Filtrar mis tareas por estado");
+      System.out.println("4. Actualizar mis tareas");
+      System.out.println("5. Cerrar sesión");
+    }
+
+    public void crearTareaPropia() {
+      
+    }
+
+    public void verMisTareas() {
+    }
+  }
+   
