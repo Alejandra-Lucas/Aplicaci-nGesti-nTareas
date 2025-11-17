6e4653d7d5657c481506dd61f3475e54139a9d66
itzeelcontrerasss-png <itzeelcontrerasss@gmail.com>
2025-11-16 22:15:26 -0600

    Update Invitado.java

--- Invitado.java
+++ Invitado.java
@@ -1,1 +1,21 @@
+java
 
+  public class Invitado extends Usuario {
+
+    public Invitado(String id, String nombre, String nickname. String correo, String password) {
+      super(id, nombre, nickname, correo, password)
+        }
+
+    @Override
+    public void mostrarMenu() {
+      System.out.println("===MENÚ INVITADO");
+      System.out.println("1. Ver todas las tareas");
+      System.out.println("2. Filtrar las tareas por estado");
+      System.out.println("3. Filtrar tareas por usuarios");
+      System.out.println("4. Cerrar sesión");
+    }
+
+    public void verTareas(){
+
+    }
+  }
